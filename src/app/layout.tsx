0a416bf93bd7c8e1b3cdb26c07fb069182a194d1
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-05-06 12:25:31 +0900

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,7 @@ export default function RootLayout({
         <Header />
 
         {children}
-        <footer>Footer</footer>
+        <Footer />
       </body>
     </html>
   );
