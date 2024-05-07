a5f08d6c51cff9d8d4accbca7342647af58c4684
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-05-07 19:50:04 +0900

    add tailwind css

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const ArticleList = () => {
   return (
     <div>
-      <article>
-        <Link href="#">
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-75">
           <Image
             src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
             alt=""
@@ -14,17 +14,61 @@ const ArticleList = () => {
             height={300}
           />
         </Link>
-        <div>
-          <Link href="#">Technology</Link>
-          <Link href="#">book</Link>
-          <p>By aaaxxx, Published on 2024/07/17</p>
-          <Link href="#">
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+          >
+            book
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">
+            By aaaxxx, Published on 2024/07/17
+          </p>
+          <Link href="#" className="text-slate-900 pb-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
             blanditiis officia perspiciatis dolores vero, deserunt distinctio
             consectetur minima! Dignissimos quia nobis doloremque aut sequi
             eaque in maxime excepturi ullam dolor.
           </Link>
-          <Link href="#">続きを読む</Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
+        </div>
+      </article>
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-75">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+          >
+            book
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">
+            By aaaxxx, Published on 2024/07/17
+          </p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            blanditiis officia perspiciatis dolores vero, deserunt distinctio
+            consectetur minima! Dignissimos quia nobis doloremque aut sequi
+            eaque in maxime excepturi ullam dolor.
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
         </div>
       </article>
     </div>
