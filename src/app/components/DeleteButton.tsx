68ead2960a13493061c5c6403d3db4d44abbb56a
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-06-15 11:26:14 +0900

    delete function

--- src/app/components/DeleteButton.tsx
+++ src/app/components/DeleteButton.tsx
@@ -1,9 +1,31 @@
 "use client";
 
+import { deleteArticle } from "@/blogApi";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const DeleteButton = () => {
-  return <div>DeleteButton</div>;
+type DeleteButtonProps = {
+  id: string;
+};
+
+const DeleteButton = ({ id }: DeleteButtonProps) => {
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    await deleteArticle(id);
+
+    router.push("/");
+    router.refresh();
+  };
+
+  return (
+    <div
+      className="bg-red-500 hover:bg-red-600 rounded-md py-2 px-5 inline cursor-pointer"
+      onClick={handleDelete}
+    >
+      削除
+    </div>
+  );
 };
 
 export default DeleteButton;
